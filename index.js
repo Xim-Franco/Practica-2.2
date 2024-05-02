f3f676c1efd4ecf5384c5543057319c6e601280f
Mon <monsesuju99@gmail.com>
2024-05-01 22:04:39 -0600

    todos los cambios aaaa

--- index.js
+++ index.js
@@ -10,12 +10,47 @@ const port = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const fortunes = [
-    "Estas líneas se cambian",
-    "por los",
-    "URL de las",
-    "imágenes que van a estar",
-    "cambiando"
+    "Catterpie",
+    "Gastly",
+    "Metapod",
+    "Pidgey",
+    "Magikarp"
 ];
+
+const pokemones = [
+    { 
+        "image": "/img/catterpie.png", 
+        "url" : "http://localhost:3000/img/catterpie.png", 
+        "frase" : "eres buena onda, pero te hablan feo y lloras"
+    },
+    { 
+        "image": "/img/chikorita.jpg", 
+        "url" : "http://localhost:3000/img/chikorita.jpg",
+        "frase" : "Eres cool, aunque no todos lo sepan" 
+    },
+    { 
+        "image": "/img/gastly.jpg", 
+        "url" : "http://localhost:3000/img/gastly.jpg",
+        "frase" : "Quizá cuando crezcas derrotes a una entrenadora psiquica" 
+    },
+    { 
+        "image": "/img/hawnter.jpg", 
+        "url" : "http://localhost:3000/img/hawnter.jpg",
+        "frase" : "Llegas tardisímo a las fiestas" 
+    },
+    { 
+        "image": "/img/piplup.png", 
+        "url" : "http://localhost:3000/img/piplup.png",
+        "frase" : "La persona más adorable del mundo" 
+    }
+    
+    
+
+    
+    
+    
+    
+]
 
 // Configurar handlebars como motor de vistas
 app.engine('handlebars', engine({
@@ -23,24 +58,28 @@ app.engine('handlebars', engine({
 }));
 app.set('view engine', 'handlebars');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static((__dirname + '/public')));
 
 // Ruta raíz
-app.get('/', (req, res) => res.render('home'));
+app.get('/', (req, res) => res.render('home'))
 
-// Ruta personalizada
-app.get('/custom-route', (req, res) => {
+
+// Ruta about
+app.get('/about', (req, res) => {
     // Código para manejar la ruta personalizada
-    res.send('Ruta personalizada');
+    const randomFortune=fortunes[Math.floor(Math.random()*fortunes.length)]
+    res.render('about',{fortune: randomFortune})
 });
 
+// Ruta personalizada
+app.get("/ruta1", (req,res)=>{
+    res.render('ruta1')
+})
+
 // Ruta para contenido dinámico
-app.get('/dynamic-content', (req, res) => {
-    const data = {
-        image: '/images/example.jpg',
-        url: 'https://example.com'
-    };
-    res.render('dynamic', { data });
+app.get('/ruta2', (req, res) => {
+    const randomPokemones=pokemones[Math.floor(Math.random()*pokemones.length)]
+    res.render('ruta2',{pokemones: randomPokemones});
 });
 
 // Ruta para manejar errores 404 (Not Found)
